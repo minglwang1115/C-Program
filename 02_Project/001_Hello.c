238c9e88585c87e0020dabeadb808978954785ca
minglwang <347411225@qq.com>
2020-02-12 10:13:36 +0800

    0212

---
+++ 02_Project/001_Hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+
+int main()
+{
+	printf("Hello World\n");
+	printf("%d", 12+21);  
+	return 0;
+}
---
+++ 02_Project/002_variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+	int price = 0;
+	const int AMOUNT = 100;
+	printf("«Î ‰»ÎΩ∂Ó");
+	scanf("%d", &price);
+	int change = AMOUNT - price;
+	printf("’“¡„%d", change);
+	return 0;
+}

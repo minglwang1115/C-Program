0e8532ed083eec306b7dfef53fb1a80695630dba
minglwang <347411225@qq.com>
2020-03-17 17:12:14 +0800

    add

--- 02_Project/practice_001.c
+++ 02_Project/practice_001.c
@@ -12,10 +12,21 @@ int main() {
 //	printf("%d\n",s-a);
 //	printf("%d\n",sizeof(a));
 	
-	char a,b,c,d;
-	scanf("%c %c",&a,&b);
-	c=getchar();
-	d=getchar();
-	printf("%c%c%c%c\n",a,b,c,d);
+//	char a,b,c,d;
+//	scanf("%c %c",&a,&b);
+//	c=getchar();
+//	d=getchar();
+//	printf("%c%c%c%c\n",a,b,c,d);
+
+	//测试两个浮点数是否相等 
+//	float a = 1.02111;
+//	float b = 1.02111;
+//	printf("%f\n",a);
+//	printf("%f\n",b);
+//	printf("%d\n",a == b);
+
+	//double类型取非
+	double a = 2.0;
+	printf("!a=%d\n",!a); 
 	return 0;
 }
